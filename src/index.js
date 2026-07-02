8df96079f50bc53fafc58932426c5e8037135a6c
sohaibmk144-dotcom <sohaibmk144@gmail.com>
2026-07-02 02:37:10 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WinkStore from './WinkStore';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<WinkStore />);
